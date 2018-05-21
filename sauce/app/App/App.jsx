23cb032f9849266b1c3c9cf2e6c95b1767681133
DostoKhan <dostokhan@gmail.com>
2018-05-22 03:04:35 +0600

    google analytics added

--- sauce/app/App/App.jsx
+++ sauce/app/App/App.jsx
@@ -4,15 +4,22 @@ import styled, { ThemeProvider } from 'styled-components';
 // import { Provider } from 'react-redux';
 import normalize from 'normalize.css';
 import milligram from 'milligram';
+import GoogleAnalytics from 'react-ga';
 
 // import create from 'redux/create';
 import theme from 'ui/theme';
+import {
+  GA_TRACKING_ID,
+} from 'helpers/constants';
 
 import './App.css';
 
 import Header from './Header/Header';
 // import Main from './Main/Main';
 
+if (PRODUCTION) {
+  GoogleAnalytics.initialize(GA_TRACKING_ID);
+}
 
 // # #222629;
 const AppBody = styled.div`
