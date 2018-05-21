db762b2caec21e922ad8068525ce5a67baded714
DostoKhan <dostokhan@gmail.com>
2018-05-22 04:08:54 +0600

    analytics initialzation code seperated

--- sauce/app/App/App.jsx
+++ sauce/app/App/App.jsx
@@ -1,27 +1,22 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 // import { BrowserRouter } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 // import { Provider } from 'react-redux';
 import normalize from 'normalize.css';
 import milligram from 'milligram';
-import GoogleAnalytics from 'react-ga';
 
 // import create from 'redux/create';
 import theme from 'ui/theme';
 import {
-  GA_TRACKING_ID,
-} from 'helpers/constants';
+  initGA,
+  logPageView,
+} from 'utils/analytics';
 
 import './App.css';
 
 import Header from './Header/Header';
 // import Main from './Main/Main';
 
-if (PRODUCTION) {
-  GoogleAnalytics.initialize(GA_TRACKING_ID);
-}
-
-// # #222629;
 const AppBody = styled.div`
   background: ${props => props.theme.colors.background};
   color: ${props => props.theme.colors.text};
@@ -33,16 +28,27 @@ const AppBody = styled.div`
 
 // const store = create();
 
-const App = ({ children }) =>
-  (
-    <ThemeProvider theme={theme}>
-      <AppBody>
-        <Header />
-        { children }
-      </AppBody>
-    </ThemeProvider>
-  );
+class App extends PureComponent {
+  componentDidMount() {
+    if (PRODUCTION) {
+      if (!window.GA_INITIALIZED) {
+        initGA();
+        window.GA_INITIALIZED = true;
+      }
+      logPageView();
+    }
+  }
 
-// <Main />
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <AppBody>
+          <Header />
+          { this.props.children }
+        </AppBody>
+      </ThemeProvider>
+    );
+  }
+}
 
 export default App;
---
+++ sauce/app/utils/analytics.js
@@ -0,0 +1,28 @@
+import ReactGA from 'react-ga';
+
+import {
+  GA_TRACKING_ID,
+} from 'helpers/constants';
+
+export const initGA = () => {
+  console.log('GA init');
+  ReactGA.initialize(GA_TRACKING_ID);
+};
+
+export const logPageView = () => {
+  console.log(`Logging pageview for ${window.location.pathname}`);
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
+export const logEvent = (category = '', action = '') => {
+  if (category && action) {
+    ReactGA.event({ category, action });
+  }
+};
+
+export const logException = (description = '', fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal });
+  }
+};
