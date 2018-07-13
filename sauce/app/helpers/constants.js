5ddb7a5d9eed355c16c48cb01064c9369ec3d1f4
DostoKhan <dostokhan@gmail.com>
2018-07-13 20:23:08 +0600

    google analytics now usage imonir.com account

--- sauce/app/helpers/constants.js
+++ sauce/app/helpers/constants.js
@@ -1,4 +1,4 @@
 export const FACEBOOK_APPID = '476512526138466'; // dostokhan imonir.com app
 // export const FACEBOOK_APPID = '534974206861491'; // imonir.dev
 //
-export const GA_TRACKING_ID = 'UA-119671429-1';
+export const GA_TRACKING_ID = 'UA-122292202-1';
